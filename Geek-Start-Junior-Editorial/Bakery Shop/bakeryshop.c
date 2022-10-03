85b6992440570befdee77bd820f68dc6de32ced6
hashitapusapati <114938888+hashitapusapati@users.noreply.github.com>
2022-10-03 22:01:42 +0530

    added bakeryshop program in c

---
+++ Geek-Start-Junior-Editorial/Bakery Shop/bakeryshop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int r;
+    float pi=3.14,a;
+    scanf("%d",&r);
+    a=pi*r*r;
+    printf("%.2f",a);
+    
+    
+    
+    return 0;
+}
