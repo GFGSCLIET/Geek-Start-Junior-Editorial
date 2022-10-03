21a62024c2995963afb3cd9eb9c0775e05c9389f
GFGSCLIET <114823075+GFGSCLIET@users.noreply.github.com>
2022-10-03 19:39:19 +0530

    Update solution.java

--- Geek-Start-Junior-Editorial/Bakery Shop/solution.java
+++ Geek-Start-Junior-Editorial/Bakery Shop/solution.java
@@ -6,8 +6,7 @@ public class Solution {
     public static void main(String[] args) {
         
         int r;
-        double
-            pi = 3.14;
+        double pi = 3.14;
         double area;
         Scanner s = new Scanner(System.in);
         r = s.nextInt();
