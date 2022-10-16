8cc264f1005a79055fc7f1b7259deffa0f60f0b5
GFGSCLIET <114823075+GFGSCLIET@users.noreply.github.com>
2022-10-16 16:08:58 +0530

    Create Solution.cpp

---
+++ Geek-Start-Junior-Editorial/Run Back Again/Solution.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()    
+{    
+int n,r,sum=0,temp;       
+cin>>n;    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=(sum*10)+r;    
+n=n/10;    
+}    
+if(temp==sum)    
+cout<<"True ";    
+else    
+cout<<"False";   
+return 0;  
+}   
